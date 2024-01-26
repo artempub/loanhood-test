9418943c17ecf00cac11a36d1115db766d581fb4
public-learner <bystevenho@gmail.com>
2024-01-26 14:18:13 -0500

    update: CardUI

--- src/components/ShopItem/ItemCard.tsx
+++ src/components/ShopItem/ItemCard.tsx
@@ -16,7 +16,7 @@ const ItemCard: React.FC<ShopIteamCardProps> = ({ item }) => {
 
     return (
         <>
-            <div className="max-w-lg rounded overflow-hidden shadow-lg m-10">
+            <div className="max-w-lg overflow-hidden  m-10 border-2 border-black hover:cursor-pointer hover:shadow-md hover:shadow-[#e9f97a] duration-150">
                 <img src={imgUrl} className="h-[300px] m-auto" alt={item.name} />
                 <div className="px-6 py-4">
                     <div className="flex justify-between">
@@ -28,7 +28,7 @@ const ItemCard: React.FC<ShopIteamCardProps> = ({ item }) => {
                     </p>
                 </div>
                 <div className="px-6 pt-4 pb-2">
-                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                    <span className="inline-block bg-[#e9f97a] border-2 border-black px-3 py-1 text-sm font-semibold text-black mr-2 mb-2">
                         {item.category}
                     </span>
                 </div>
