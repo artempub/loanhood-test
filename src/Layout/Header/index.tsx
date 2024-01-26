5b6b04a76d9fad2c1be5f601bc8a7d945af9fd31
public-learner <bystevenho@gmail.com>
2024-01-26 13:53:28 -0500

    update: Header-UI

--- src/Layout/Header/index.tsx
+++ src/Layout/Header/index.tsx
@@ -2,7 +2,7 @@ const Header: React.FC = () => {
 
     return (
         <>
-            <div className="bg-[#e9f97a] h-[100px] text-black justify-center flex items-center text-3xl font-extrabold shadow-sm">
+            <div className="bg-[#e9f97a] border-b-2  border-t-2 border-black h-[100px] text-black flex items-center text-5xl font-bold shadow-sm p-5">
                 LOANHOOD
             </div>
         </>
