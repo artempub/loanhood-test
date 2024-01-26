25f0fc45bbe8658e45ddf87e1b8fafed79608d1a
public-learner <bystevenho@gmail.com>
2024-01-26 17:19:06 -0500

    update: server_env

--- src/services/Api.ts
+++ src/services/Api.ts
@@ -1,7 +1,7 @@
 import axios, {AxiosRequestConfig, AxiosResponse } from "axios";
 
 const instance = axios.create({
-    baseURL: 'http://localhost:3001',
+    baseURL: process.env.REACT_APP_SERVER_URL,
 })
 
 export const api = {
