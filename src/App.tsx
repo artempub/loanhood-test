063cefeb5f1228339ca8f66744001c6f34ddb11f
public-learner <bystevenho@gmail.com>
2024-01-26 14:05:41 -0500

    Footer: add Footer

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './Layout/Header';
 import Listing from './pages/Listing';
+import Footer from './Layout/Footer';
 
 function App() {
   return (
     <>
     <Header />
     <Listing/>
+    <Footer />
     </>
   );
 }
